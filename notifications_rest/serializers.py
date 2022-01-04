90dd01af56b7023ad72e5f524c343ecc8d8f5d0e
Solaris <97111981+Solaris-bi@users.noreply.github.com>
2022-01-04 16:23:51 +0100

    Update NotificationSerializer, Omit permissions agent and groups

--- notifications_rest/serializers.py
+++ notifications_rest/serializers.py
@@ -1,18 +1,23 @@
 from rest_framework.serializers import ModelSerializer, RelatedField
 from rest_framework import serializers
+from rest_flex_fields import FlexFieldsModelSerializer
 from notifications.models import Notification
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth import get_user_model
+from django.conf import settings
 
-UserModel = get_user_model()
+if hasattr(settings, 'REST_NOTIFICATION_SERIALIZER_CLASS'):
+    from pydoc import locate
+    UserSerializer = locate(settings.REST_NOTIFICATION_SERIALIZER_CLASS)
+else:
+    from django.contrib.auth import get_user_model
+    UserModel = get_user_model()
+    
+    class UserSerializer(ModelSerializer):
+        id = serializers.IntegerField()
 
-
-class UserSerializer(ModelSerializer):
-    id = serializers.IntegerField()
-
-    class Meta:
-        model = UserModel
-        fields = ['id', ]
+        class Meta:
+            model = UserModel
+            fields = ['id', ]
 
 
 class ContentTypeSerializer(ModelSerializer):
@@ -33,8 +38,8 @@ class GenericNotificationRelatedField(RelatedField):
 
 
 class NotificationSerializer(ModelSerializer):
-    recipient = UserSerializer()
-    actor = UserSerializer()
+    recipient = UserSerializer(omit=["permissions", "agent", "groups"])
+    actor = UserSerializer(omit=["permissions", "agent", "groups"])
     verb = serializers.CharField()
     level = serializers.CharField()
     description = serializers.CharField()
